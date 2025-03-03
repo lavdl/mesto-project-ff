1aeacfb92aa8d89567c124b62295e34168286462
lavdl <maxssw8@gmail.com>
2025-03-03 08:25:32 +0400

    upload

--- scripts/cards.js
+++ scripts/cards.js
@@ -4,20 +4,20 @@ const initialCards = [
       link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
     },
     {
-      name: "Челябинская область",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg",
+      name: "Александров",
+      link: "https://i.ibb.co/HTtJ3J4Z/alexandrovmain.jpg",
     },
     {
       name: "Иваново",
       link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg",
     },
     {
-      name: "Камчатка",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg",
+      name: "Ижевск",
+      link: "https://i.ibb.co/ZzpFzJLB/f-a-W1n-Lmdlb-Glvc-Ghvd-G8u-Y29t-L2l6a-GV2c2sv-MDJfa-Xpo-ZXZzay5qc-Gc-X19p-ZD0x-NDg0-Mzg.jpg",
     },
     {
-      name: "Холмогорский район",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg",
+      name: "Рим",
+      link: "https://images.unsplash.com/photo-1549835445-65c77ce113e6?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     },
     {
       name: "Байкал",
--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,24 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+
+const cardTemplate = document.querySelector("#card-template").content;
+const cardContainer = document.querySelector(".places__list");
+function loadCards(initialCards, deleteCard) {
+  initialCards.forEach((element) => {
+    const cardElement = cardTemplate
+      .querySelector(".places__item")
+      .cloneNode(true);
+    cardElement.querySelector(".card__image").src = element.link;
+    cardElement.querySelector(".card__title").textContent = element.name;
+    const deleteButton = cardElement.querySelector(".card__delete-button");
+    deleteButton.addEventListener("click", () => {
+      deleteCard(cardElement);
+    });
+    cardContainer.append(cardElement);
+  });
+};
+const deleteCard = (cardElement) => {
+  cardElement.closest(".places__item").remove();
+};
+loadCards(initialCards, deleteCard);
