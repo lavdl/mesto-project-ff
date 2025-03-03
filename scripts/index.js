5896331bb99577d57d0d590c1832525fb964724d
lavdl <maxssw8@gmail.com>
2025-03-03 12:22:54 +0400

    fix rev

--- scripts/index.js
+++ scripts/index.js
@@ -10,20 +10,27 @@
 
 const cardTemplate = document.querySelector("#card-template").content;
 const cardContainer = document.querySelector(".places__list");
+function createCard(element, deleteCard) {
+  const cardElement = cardTemplate
+    .querySelector(".places__item")
+    .cloneNode(true);
+  const cardImage = cardElement.querySelector(".card__image");
+  const cardTitle = cardElement.querySelector(".card__title");
+  const deleteButton = cardElement.querySelector(".card__delete-button");
+  cardImage.src = element.link;
+  cardImage.alt = `Изображение места: ${element.name}`;
+  cardTitle.textContent = element.name;
+  deleteButton.addEventListener("click", () => {
+    deleteCard(cardElement);
+  });
+  return cardElement;
+}
 function loadCards(initialCards, deleteCard) {
   initialCards.forEach((element) => {
-    const cardElement = cardTemplate
-      .querySelector(".places__item")
-      .cloneNode(true);
-    cardElement.querySelector(".card__image").src = element.link;
-    cardElement.querySelector(".card__title").textContent = element.name;
-    const deleteButton = cardElement.querySelector(".card__delete-button");
-    deleteButton.addEventListener("click", () => {
-      deleteCard(cardElement);
-    });
+    const cardElement = createCard(element, deleteCard);
     cardContainer.append(cardElement);
   });
-};
+}
 const deleteCard = (cardElement) => {
   cardElement.closest(".places__item").remove();
 };
